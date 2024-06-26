c097c3426668738e39d516205ba43d7455668073
BastianYakha <bastian@yakha.eu>
2024-06-26 15:34:56 +0200

    update

--- ClassLibrary1/Models.cs
+++ ClassLibrary1/Models.cs
@@ -15,21 +15,6 @@
         public DateTime LastUpdate { get; set; }
     }
 
-
-    //the class below is just an example and needs to be changed
-    public class DataUpdater
-    {
-        //logic for updating WaterLevel, Temperature, RainAmount every 15 min in river object
-        private River river;
-        public DataUpdater(River river)
-        {
-            this.river = river;
-        }
-
-        public void Update()
-        { }
-    }
-
     public class RiverCreator
     {
         Random rnd = new Random();
