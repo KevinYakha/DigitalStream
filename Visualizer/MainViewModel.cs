320b52d89f882736b977cabd70d05dad9dc49a30
KevinYakha <kevin@yakha.eu>
2024-06-26 14:29:34 +0200

    Initial setup with a basic plot example

---
+++ Visualizer/MainViewModel.cs
@@ -0,0 +1,16 @@
+﻿using OxyPlot;
+using OxyPlot.Series;
+
+namespace Visualizer
+{
+    internal class MainViewModel
+    {
+        public MainViewModel()
+        {
+            this.riverPlot = new PlotModel { Title = "Example" };
+            this.riverPlot.Series.Add(new FunctionSeries(Math.Cos, 0, 10, 0.1, "cos(x)"));
+        }
+
+        public PlotModel riverPlot {  get; private set; }
+    }
+}
--- Visualizer/MainWindow.xaml.cs
+++ Visualizer/MainWindow.xaml.cs
@@ -19,6 +19,7 @@ namespace Visualizer
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = new MainViewModel();
         }
     }
 }
