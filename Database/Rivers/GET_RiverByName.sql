e9747d828a0d536c664569e73094776894731a6f
LisaK <lisa@yakha.eu>
2024-06-26 23:09:17 +0200

    riverdata variables removed from river tables and procedures

--- Database/Rivers/GET_RiverByName.sql
+++ Database/Rivers/GET_RiverByName.sql
@@ -2,7 +2,7 @@
 @Name NVARCHAR(250)
  AS
 BEGIN
-    SELECT Id, Name, WaterLevel, Temperature, RainAmount, FloodLevel, LastUpdate
+    SELECT Id, Name, FloodLevel, LastUpdate
     FROM Rivers
      WHERE Name = @Name;
 END
--- Database/Rivers/Rivers.sql
+++ Database/Rivers/Rivers.sql
@@ -1,9 +1,6 @@
 ﻿CREATE TABLE [dbo].[Rivers] (
     [Id]          UNIQUEIDENTIFIER NOT NULL,
     [Name]        NVARCHAR (100)   NULL,
-    [WaterLevel]  FLOAT (53)       NULL,
-    [Temperature] FLOAT (53)       NULL,
-    [RainAmount]  FLOAT (53)       NULL,
     [FloodLevel]  FLOAT (53)       NULL,
     [LastUpdate]  DATETIME         NULL,
     PRIMARY KEY CLUSTERED ([Id] ASC)
